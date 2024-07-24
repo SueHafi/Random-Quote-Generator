8f1652e868f94b4a899a2a86b57c59186e8cd249
sue rose <suehaizoglu@gmail.com>
2024-07-24 22:15:22 +0100

    initiated backend server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
